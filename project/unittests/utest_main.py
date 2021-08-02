32208816befbf35afd2b2d6ecf847cde6bb82a45
Kirill Fursaev <kirill.fursaev@gmail.com>
2021-08-03 02:38:18 +0300

    Незначительные изменения, не влияющие на логику и функционал кода.

--- project/unittests/utest_main.py
+++ project/unittests/utest_main.py
@@ -9,24 +9,24 @@ class MpAvgFlightTimeTest(TestCase):
     def tearDown(self) -> None:
         pass
 
-    def test_first_arg_not_a_str(self):
+    def test_first_arg_not_a_str(self) -> None:
         self.assertRaises(TypeError, mp_avg_flight_time, (12, 'str'))
 
-    def test_second_arg_not_a_string(self):
+    def test_second_arg_not_a_string(self) -> None:
         self.assertRaises(TypeError, mp_avg_flight_time, ('12', 12))
 
-    def test_first_arg_is_in_incorrect_format(self):
+    def test_first_arg_is_in_incorrect_format(self) -> None:
         self.assertRaises(TypeError, mp_avg_flight_time, ('12-1333-313', '2012-03-05 10:12:31'))
 
-    def test_second_arg_is_in_incorrect_format(self):
+    def test_second_arg_is_in_incorrect_format(self) -> None:
         self.assertRaises(TypeError, mp_avg_flight_time, ('2012-03-05 10:12:31', '2012-03-05 10:12:91'))
 
-    def test_everything_is_correct(self):
+    def test_everything_is_correct(self) -> None:
         date1 = '2020-06-13 20:06:13'
         date2 = '2019-05-12 19:05:12'
         self.assertEqual(type(mp_avg_flight_time(date1, date2)), float)
 
-    def first_arg_is_bigger_than_second(self):
+    def first_arg_is_bigger_than_second(self) -> None:
         date1 = '2019-05-12 19:05:12'
         date2 = '2020-06-13 20:06:13'
         self.assertRaises(ValueError, mp_avg_flight_time, (date1, date2))
@@ -42,16 +42,16 @@ class MpDelayTimeTest(TestCase):
     def test_first_arg_not_a_str(self) -> None:
         self.assertRaises(TypeError, mp_delay_time, (12, 'str'))
 
-    def test_second_arg_not_a_string(self):
+    def test_second_arg_not_a_string(self) -> None:
         self.assertRaises(TypeError, mp_delay_time, ('12', 12))
 
-    def test_first_arg_is_in_incorrect_format(self):
+    def test_first_arg_is_in_incorrect_format(self) -> None:
         self.assertRaises(TypeError, mp_delay_time, ('12-1333-313', '2012-03-05 10:12:31'))
 
-    def test_second_arg_is_in_incorrect_format(self):
+    def test_second_arg_is_in_incorrect_format(self) -> None:
         self.assertRaises(TypeError, mp_delay_time, ('2012-03-05 10:12:31', '2012-03-05 10:12:91'))
 
-    def test_everything_is_correct(self):
+    def test_everything_is_correct(self) -> None:
         date1 = '2020-06-13 20:06:13'
         date2 = '2019-05-12 19:05:12'
         self.assertEqual(type(mp_delay_time(date1, date2)), float)
