ef8a1e251f396ee2ec62c15cd87d455467195d33
Kirill Fursaev <kirill.fursaev@gmail.com>
2021-08-03 02:03:19 +0300

    Docstring метода mp_avg_flight_time() дополнена описанием выбрасываемого исключения. Добавлена проверка на вычитание ранней даты из более поздней (во избежание неотслеживаемого получения отрицательных значений времени полета).

--- project/main.py
+++ project/main.py
@@ -170,12 +170,14 @@ def mp_avg_flight_time(arr: str, dep: str) -> float:
     Вспомогательный метод для анализа среднего времени полета
     :param arr: Время вылета борта
     :param dep: Время прилета борта
-    :raises: TypeError
+    :raises: TypeError, ValueError
     :return: Время полета в секундах
     """
     diff = datetime.datetime.strptime(arr, '%Y-%m-%d %H:%M:%S') \
         - datetime.datetime.strptime(dep, '%Y-%m-%d %H:%M:%S')
     diff = diff.total_seconds()
+    if diff < 0:
+        raise ValueError('В данном методе не предусмотрено вычитания поздней даты из более ранней')
     return diff
 
 
@@ -184,6 +186,7 @@ def mp_delay_time(dep_plan: str, dep_act: str) -> float:
     Вспомогательный метод для расчета разницы в запланированном и реальном времени вылета борта
     :param dep_plan: Запланированное время вылета борта
     :param dep_act: Реальное время вылета борта
+    :raises: TypeError
     :return: Разность между запланированным и реальным временем вылета в секундах
     """
     dep_plan, dep_act = datetime.datetime.strptime(dep_plan, '%Y-%m-%d %H:%M:%S'), \
