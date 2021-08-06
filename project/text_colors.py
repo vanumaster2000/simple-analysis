d6a5a9aa587cc7b27b4be87a3d5b51bac0513d44
Kirill Fursaev <kirill.fursaev@gmail.com>
2021-08-06 15:02:25 +0300

    Добавлен файл с ANSI-последовательностями для форматирования вывода информации.

---
+++ project/text_colors.py
@@ -0,0 +1,10 @@
+class Colors:
+    HEADER = '\033[95m'
+    ENDC = '\033[0m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
