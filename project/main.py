db57aa7abc3dc8d8166c10ee7540c16606a0a38f
Kirill Fursaev <kirill.fursaev@gmail.com>
2021-08-06 13:59:21 +0300

    Первый информационный блок реструктурирован. Теперь в одной секции выводится информация о количестве судов по производителю, для каждого из них - информация по местам в каждом из классов. Код реформатирован.

--- project/main.py
+++ project/main.py
@@ -22,8 +22,11 @@ def planes_data(planes_dataframe: pd.DataFrame) -> None:
     """
     if type(planes_dataframe) != pd.DataFrame:
         raise TypeError('Метод предназначен для обработки pandas.Dataframe')
+
     print(filler('='))
+
     print('ИСПОЛЬЗУЕМЫЙ ФЛОТ АВИАСУДОВ')
+
     planes_dataframe = planes_dataframe.drop(['range'], axis=1).assign(Economy=0, Comfort=0, Business=0)
     seats = pd.read_sql("SELECT * FROM seats", connection).drop(['seat_no'], axis=1)  # Перечень мест для каждого борта
     seats_data = seats.groupby('aircraft_code')['fare_conditions'].value_counts()
@@ -39,30 +42,27 @@ def planes_data(planes_dataframe: pd.DataFrame) -> None:
                planes_dataframe['Comfort'],
                planes_dataframe['Business'])
            ]  # Список кортежей вида (название судна, мест в экономе, мест в комфорте, мест в бизнесе)
-    types = sorted(res, key=lambda x: x[0])
-    print('Модели самолетов, находящихся в использовании:')
-    for board in types:
-        print('\t', board[0], '\n\t  Места:', sep='')
-        (eco, com, bus) = board[1:]
-        if eco > 0:
-            print(f'\t\tЭконом-класс: {eco}')
-        if com > 0:
-            print(f'\t\tКомфорт-класс: {com}')
-        if bus > 0:
-            print(f'\t\tБизнесс-класс: {bus}')
-    types = [
-        x[0].split()[0].lower() for x in types
-    ]  # Приведение производителя (первое слово в строке) к нижнему регистру
-    aircraft_by_producers = {x.title(): types.count(x) for x in air_prod}
-    aircraft_by_producers = [(x, aircraft_by_producers[x]) for x in aircraft_by_producers.keys() if
-                             aircraft_by_producers[x] > 0]
-    aircraft_by_producers = {x: y for (x, y) in aircraft_by_producers}
+    types = sorted(set(res), key=lambda x: x[0])
+    prods = [
+        x[0].split()[0] for x in types
+    ]  # Получение списка производителей
+    aircraft_by_producers = {x: [prods.count(x)] for x in air_prod if prods.count(x) > 0}
+    for producer in aircraft_by_producers.keys():
+        for board in types:
+            if board[0].startswith(producer):
+                aircraft_by_producers[producer].append(board)
 
-    print(filler('-'))
-
-    print('Количество используемых самолетов по производителям:')
     for (producer, amount) in aircraft_by_producers.items():
-        print(f'\t{producer}: {amount} ед.')
+        print(f'  {producer}: {amount[0]} ед.')
+        for i in range(1, len(amount)):
+            print(f'\t{i}) {amount[i][0]}\n\t  Места:')
+            (eco, com, bus) = amount[i][1:]
+            if eco > 0:
+                print(f'\t\tЭконом-класс: {eco}')
+            if com > 0:
+                print(f'\t\tКомфорт-класс: {com}')
+            if bus > 0:
+                print(f'\t\tБизнесс-класс: {bus}')
 
     print(filler('='))
 
@@ -77,8 +77,10 @@ def flights_data(flights_dataframe: pd.DataFrame) -> None:
     """
     if type(flights_dataframe) != pd.DataFrame:
         raise TypeError('Метод предназначен для обработки pandas.Dataframe')
+
     print("ПОЛЕТЫ")
     print(f'Среднее время полета на основе {len(flights_dataframe)} записей:')
+
     # Получение полных перечней времени взлета и посадки бортов и отсечение нулевого смещения по часовому поясу
     departure_actual = [str(x)[:-6] for x in flights_dataframe[['actual_departure']]['actual_departure']]
     arrival_actual = [str(x)[:-6] for x in flights_dataframe[['actual_arrival']]['actual_arrival']]
@@ -105,7 +107,6 @@ def flights_data(flights_dataframe: pd.DataFrame) -> None:
 
     avg_delay_time = str(datetime.timedelta(seconds=np.average(delayed))).split('.')[0]
     flights_dataframe['delay'] = delay_time
-
     res = f'ИЗ {len(departure_actual)} совершенных рейсов\n' \
           f'\tВовремя вылетели: {in_time}'
     if in_time > 0:
@@ -120,8 +121,10 @@ def flights_data(flights_dataframe: pd.DataFrame) -> None:
     if len(too_soon) > 0:
         too_soon_percent = "{:.3%}".format(len(too_soon) / len(departure_actual))
         res += f' ({too_soon_percent}.'
+
     print(res)
     print(filler('-'))
+
     df_with_seconds = flights_dataframe.drop(
         ['scheduled_departure', 'scheduled_arrival', 'actual_departure', 'actual_arrival',
          'flight_id', 'aircraft_code', 'status'], axis=1)
@@ -200,25 +203,34 @@ def tickets_data(tickets_dataframe: pd.DataFrame) -> None:
     """
     if type(tickets_dataframe) != pd.DataFrame:
         raise TypeError('Метод предназначен для обработки pandas.Dataframe')
+
     print('БИЛЕТЫ')
+
     economy_tickets = tickets_dataframe.loc[tickets_dataframe['type'] == 'Economy']
     economy_tickets_amount = len(economy_tickets)
     avg_price = economy_tickets['price'].sum() / economy_tickets_amount
+
     print(f'Средняя цена билета:\n'
           f'\tВ эконом-класс: {"{:.2f}".format(avg_price)}')
+
     comfort_tickets = tickets_dataframe.loc[tickets_dataframe['type'] == 'Comfort']
     comfort_tickets_amount = len(comfort_tickets)
     avg_price = comfort_tickets['price'].sum() / comfort_tickets_amount
+
     print(f'\tВ комфорт-класс: {"{:.2f}".format(avg_price)}')
+
     business_tickets = tickets_dataframe.loc[tickets_dataframe['type'] == 'Business']
     business_tickets_amount = len(business_tickets)
     avg_price = business_tickets['price'].sum() / business_tickets_amount
+
     print(f'\tВ бизнесс-класс: {"{:.2f}".format(avg_price)}')
     print(filler('-'))
+
     total = economy_tickets_amount + business_tickets_amount + comfort_tickets_amount
     economy_percent = economy_tickets_amount / total
     comfort_percent = comfort_tickets_amount / total
     business_percent = business_tickets_amount / total
+
     print(f'Из всех билетов куплено:\n'
           f'\tВ эконом-класс: {economy_tickets_amount} ({"{:.3%}".format(economy_percent)})\n'
           f'\tВ комфорт-класс: {comfort_tickets_amount} ({"{:.3%}".format(comfort_percent)})\n'
