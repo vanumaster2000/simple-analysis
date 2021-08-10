8ac0bd7787b7e8583489f98537b699ff519903bd
Kirill Fursaev <kirill.fursaev@gmail.com>
2021-08-10 12:32:16 +0300

    Изменена структура модуля. Теперь поля класса скрыты для доступа извне во избежание конфликта при вызове синонимичного метода.

--- project/text_colors.py
+++ project/text_colors.py
@@ -1,34 +1,34 @@
 class Colors:
-    HEADER = '\033[95m'
-    END = '\033[0m'
-    BLUE = '\033[94m'
-    CYAN = '\033[96m'
-    GREEN = '\033[92m'
-    WARNING = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    _HEADER = '\033[95m'
+    _END = '\033[0m'
+    _BLUE = '\033[94m'
+    _CYAN = '\033[96m'
+    _GREEN = '\033[92m'
+    _WARNING = '\033[93m'
+    _RED = '\033[91m'
+    _BOLD = '\033[1m'
+    _UNDERLINE = '\033[4m'
 
     def header(self, text_to_wrap):
-        return self.HEADER + text_to_wrap + self.END
+        return self._HEADER + str(text_to_wrap) + self._END
 
     def bold(self, text_to_wrap):
-        return self.BOLD + text_to_wrap + self.END
+        return self._BOLD + str(text_to_wrap) + self._END
 
     def underline(self, text_to_wrap):
-        return self.UNDERLINE + text_to_wrap + self.END
+        return self._UNDERLINE + str(text_to_wrap) + self._END
 
     def blue(self, text_to_wrap):
-        return self.BLUE + text_to_wrap + self.END
+        return self._BLUE + str(text_to_wrap) + self._END
 
     def cyan(self, text_to_wrap):
-        return self.CYAN + text_to_wrap + self.END
+        return self._CYAN + str(text_to_wrap) + self._END
 
     def green(self, text_to_wrap):
-        return self.GREEN + text_to_wrap + self.END
+        return self._GREEN + str(text_to_wrap) + self._END
 
     def warning(self, text_to_wrap):
-        return self.WARNING + text_to_wrap + self.END
+        return self._WARNING + str(text_to_wrap) + self._END
 
     def red(self, text_to_wrap):
-        return self.RED + text_to_wrap + self.END
+        return self._RED + str(text_to_wrap) + self._END
