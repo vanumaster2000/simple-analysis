c203d92601a50f2818d3429afeeefcf3143de223
Kirill Fursaev <kirill.fursaev@gmail.com>
2021-08-13 03:41:27 +0300

    Добавлены docstring методов. В методах теперь есть docstring. У метода warning() название изменено на более логичное red().

--- project/text_colors.py
+++ project/text_colors.py
@@ -9,26 +9,66 @@ class Colors:
     _BOLD = '\033[1m'
     _UNDERLINE = '\033[4m'
 
-    def header(self, text_to_wrap):
+    def header(self, text_to_wrap: str) -> str:
+        """
+        Окрашивает текст в ярко-фиолетовый цвет
+        :param text_to_wrap: текст для окрашивания
+        :return: строка с ANSI-последовательностью
+        """
         return self._HEADER + str(text_to_wrap) + self._END
 
-    def bold(self, text_to_wrap):
+    def bold(self, text_to_wrap: str) -> str:
+        """
+        Возвращает текст в полужирном начертании
+        :param text_to_wrap: текст для утолщения
+        :return: строка с ANSI-последовательностью
+        """
         return self._BOLD + str(text_to_wrap) + self._END
 
-    def underline(self, text_to_wrap):
+    def underline(self, text_to_wrap: str) -> str:
+        """
+        Возвращает текст в подчеркнутом начертании
+        :param text_to_wrap: текст для подчеркивания
+        :return: строка с ANSI-последовательностью
+        """
         return self._UNDERLINE + str(text_to_wrap) + self._END
 
-    def blue(self, text_to_wrap):
+    def blue(self, text_to_wrap: str) -> str:
+        """
+        Возвращает текст в синем цвете
+        :param text_to_wrap: текст для окрашивания
+        :return: строка с ANSI-последовательностью
+        """
         return self._BLUE + str(text_to_wrap) + self._END
 
-    def cyan(self, text_to_wrap):
+    def cyan(self, text_to_wrap: str) -> str:
+        """
+        Возвращает текст в светло-голубом цвете
+        :param text_to_wrap: текст для окрашивания
+        :return: строка с ANSI-последовательностью
+        """
         return self._CYAN + str(text_to_wrap) + self._END
 
-    def green(self, text_to_wrap):
+    def green(self, text_to_wrap: str) -> str:
+        """
+        Возвращает текст в зеленом цвете
+        :param text_to_wrap: текст для окрашивания
+        :return: строка с ANSI-последовательностью
+        """
         return self._GREEN + str(text_to_wrap) + self._END
 
-    def warning(self, text_to_wrap):
+    def orange(self, text_to_wrap: str) -> str:
+        """
+        Возвращает текст в оранжевом цвете
+        :param text_to_wrap: текст для окрашивания
+        :return: строка с ANSI-последовательностью
+        """
         return self._WARNING + str(text_to_wrap) + self._END
 
-    def red(self, text_to_wrap):
+    def red(self, text_to_wrap: str) -> str:
+        """
+        Возвращает текст в красном цвете
+        :param text_to_wrap: текст для окрашивания
+        :return: строка с ANSI-последовательностью
+        """
         return self._RED + str(text_to_wrap) + self._END
